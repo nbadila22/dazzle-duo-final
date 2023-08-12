9adbe51759afbc559e1bf9e99615674eae639533
nbadila22 <140140295+nbadila22@users.noreply.github.com>
2023-08-12 11:54:25 -0400

    BUG UPDATE: corrected the GEN_Date function to replace the current string (if any), before generating and a new date suggestion. This correction avoids the p element from having to recreate/add on every time to the div. Now we are only adressing that singular paragraph assigned with the id 'dateP'

--- project.js
+++ project.js
@@ -1,6 +1,6 @@
 
 
-const dateP = document.getElementById('Date_Print_Div')
+const datePDiv = document.getElementById('Date_Print_Div')
 
 function GEN_Date() {
 // This function is used to select a random date idea from the array "dates" and return the value outside of this function for it to display 
@@ -11,8 +11,9 @@ function GEN_Date() {
 let PULL_Date = dates[(Math.floor(Math.random() * dates.length))];
 // randomizes date idea from dates array and assigns it to the returning variable "PULL_Dates"
 
-let newDate = document.createElement("p"); 
-dateP.append(PULL_Date)
+document.getElementById("dateP").innerHTML = PULL_Date;
+
+
 
 } 
 
