b1ef110abbcb4c7522f15e781ac1963716c70d0a
COR Coding <corcoding@DESKTOP-PH48BSF.localdomain>
2023-08-14 14:21:17 -0400

    did everything

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dazzle-duo-final
+Dazzle Duo is a website designed by Hien Nguyen, Sababu Barashango, Nicolas Badila, and Jaidon Christian inorder to provide fitting date ideas based on the user's own personality. We split the personality types into 2 main ones: introverts and extroverts. 
+
+The features of our site includes: appealing graphic design, a fully functioning personality test for those who do not know their own personality type, a navbar, a login form to save your date, and a button that provides the user with date ideas. 
+
+Some technologies that were used include: Bootstrap: for css styling with the nav bar and Netlify: used to make the site publicly avaliable on a domain.a
+
--- project.js
+++ project.js
@@ -37,5 +37,75 @@ document.getElementById("dateP2").innerHTML = PULL_Date;
 
 
 
+document.getElementById("quiz-form").addEventListener("submit", function(event) {
+    event.preventDefault();
 
-    
+    const responses = [];
+    const formElements = document.querySelectorAll(".question input[type=radio]:checked");
+
+    formElements.forEach(element => {
+        responses.push(element.value);
+    });
+
+   
+});
+
+function analyzePersonality() {
+    // Where values are retrieved
+    const responses = {
+        q1: document.querySelector('input[name="q1"]:checked').value,
+        q2: document.querySelector('input[name="q2"]:checked').value,
+        q3: document.querySelector('input[name="q3"]:checked').value,
+        q4: document.querySelector('input[name="q4"]:checked').value,
+        q5: document.querySelector('input[name="q5"]:checked').value
+    };
+
+    // Where we calculated the yes and no count
+    const yesCount = Object.values(responses).filter(response => response === "Yes").length;
+    const noCount = Object.values(responses).filter(response => response === "No").length;
+
+    // function to determine personality type by tallying up values of yes and no
+    let personality = "";
+    if (yesCount > noCount) {
+        personality = "Introvert";
+    } else if (noCount > yesCount) {
+        personality = "Extrovert";
+    } else {
+        personality = "Mix of Both";
+    }
+
+    // Where we display the personality result
+    const resultElement = document.getElementById("result");
+    resultElement.innerHTML = `<p>Your personality type: ${personality}</p>`;
+}
+
+            // SCROLL TO DINNER GENERATOR SECTION // 
+function scrollUpTop(){
+    document.querySelector('#head-flo').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+    // SCROLL TO VIBE CHECK SECTION // 
+function vibeScroll(){
+    document.querySelector('#vibe-check-cont').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+    // SCROLL TO WTM SECTION // 
+    function wtmScroll(){
+        document.querySelector('#wtm-cont').scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+
+        // SCROLL TO DINNER GENERATOR SECTION // 
+function dinnerScroll(){
+    document.querySelector('#drinks-cont').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+function scrollUpTop(){
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
